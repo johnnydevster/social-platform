f8b3fc765e51511beb58845081c0da2bf1f282ad
Johnny Backlund <johnnybacklund@hotmail.com>
2022-03-07 21:42:26 +0100

    Changed look of nav

--- components/layout/Nav.js
+++ components/layout/Nav.js
@@ -2,30 +2,32 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav className="h-16 bg-primary-200 flex justify-between fixed w-full font-bold text-lg text-primary-700">
-      <ul className="flex items-center ml-16">
-        <li className="">
-          <Link href="/">
-            <a className="flex items-center hover:text-primary-500 transition-all">
-              <span className="material-icons">home</span>
-              <span className="ml-1">Hem</span>
-            </a>
-          </Link>
-        </li>
-        <li className="ml-5">
-          <Link href="/annonser">
-            <a className="flex items-center hover:text-primary-500 transition-all">
-              <span className="material-icons">assignment</span>
-              <span className="ml-1">Annonser</span>
-            </a>
-          </Link>
-        </li>
-      </ul>
-      <ul className="flex items-center mr-16">
-        <li>
-          <a>Logga in</a>
-        </li>
-      </ul>
+    <nav className="h-16 bg-gray-50 flex justify-center fixed w-full font-bold text-primary-700">
+      <div className="flex h-full max-w-7xl grow">
+        <ul className="flex mx-5 space-x-6 items-center grow">
+          <li className="">
+            <Link href="/">
+              <a className="flex items-center hover:underline transition-all">
+                <span className="material-icons">home</span>
+                <span className="ml-1">Hem</span>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/annonser">
+              <a className="flex items-center hover:underline transition-all">
+                <span className="material-icons">assignment</span>
+                <span className="ml-1">Annonser</span>
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <ul className="flex space-x-6 mx-5 items-center justify-end grow">
+          <li>
+            <a>Logga in</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
