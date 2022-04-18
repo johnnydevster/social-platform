a93d0c5fa3105772daeb75fcf0f98303f0c81f83
Johnny Backlund <johnnybacklund@hotmail.com>
2022-04-18 21:36:45 +0200

    started with auth

--- components/layout/Nav.js
+++ components/layout/Nav.js
@@ -1,6 +1,10 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
+import { useCallback } from "react";
 
 export default function Nav() {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <nav className="h-16 bg-primary-800 flex justify-center fixed z-40 w-full font-bold text-primary-100">
       <div className="flex h-full max-w-7xl grow">
@@ -23,10 +27,23 @@ export default function Nav() {
           </li>
         </ul>
         <ul className="flex space-x-6 mx-5 items-center justify-end grow">
-          <a className="flex items-center hover:underline transition-all">
-            <span className="material-icons">account_circle</span>
-            <span className="ml-1">Logga in</span>
-          </a>
+          {!session ? ( // user isn't logged in
+            <button
+              onClick={() => signIn()}
+              className="flex items-center hover:underline transition-all"
+            >
+              <span className="material-icons">account_circle</span>
+              <span className="ml-1">Log in</span>
+            </button>
+          ) : (
+            <button
+              className="flex items-center hover:underline transition-all"
+              onClick={() => signOut()}
+            >
+              <span className="material-icons">account_circle</span>
+              <span className="ml-1">Log out</span>
+            </button>
+          )}
         </ul>
       </div>
     </nav>
